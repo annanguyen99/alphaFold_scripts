92a6ee01c525c5c4c7a9c892137d6b21abdebd24
annanguyen99 <hellothaomy@gmail.com>
2022-09-14 13:55:50 -0500

    add protein list

--- main.py
+++ main.py
@@ -21,7 +21,11 @@ def get_fasta(protein_list):
         else:
             print("File already exist")
 
+def create_hetero(protein_list, cas9_list):
+    print("Create hetero_file")
 if __name__ == "__main__":
-    protein_list = ["A0A142X169"]
+    protein_list = ["A0A142X169", "A0A142WY31", "A0A517U1U2", "A0A517U1I5", "A0A2W4MKW0", "A0A0B8YAX9",
+                    "A0A350V0L1", "A0A838CTC1", "A0A1G8ZP20", "A0A6N4TLR5", "A0A6N7QZV0", "A0A150L959"]
+    cas9_list = ["A0A142WV29", "A0A517TX54", ]
     get_fasta(protein_list)
     print("Done")
